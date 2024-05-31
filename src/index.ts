5b2e3a4eeee87120053515d0e33883bc5463b5dd
Brett Beutell <brbeut@gmail.com>
2024-05-31 15:47:40 +0200

    Add example drizzle query for seeded users

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { Hono } from 'hono'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { users } from './db/schema';
 
-const app = new Hono()
+type Bindings = {
+  DATABASE_URL: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/api/users', async (c) => {
+  const sql = neon(c.env.DATABASE_URL)
+  const db = drizzle(sql);
+
+  return c.json({
+    users: await db.select().from(users)
+  })
 })
 
 export default app
