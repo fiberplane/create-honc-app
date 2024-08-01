b69a76ba0f77dc51046f74fef1ff6353dc8f404d
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-08-01 10:53:04 +0200

    rename cli and package setup

---
+++ cli/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+const isDev = process.env.npm_lifecycle_event === "dev";
+
+console.log("isDev", isDev)
+console.log("process.env.npm_lifecycle_event", process.env.npm_lifecycle_event)
+
+export default defineConfig({
+	clean: true,
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	minify: !isDev,
+	target: "esnext",
+	outDir: "dist",
+	sourcemap: true,
+	onSuccess: isDev ? "node dist/index.js" : undefined,
+	noExternal: ["tar", "minipass"],
+});
