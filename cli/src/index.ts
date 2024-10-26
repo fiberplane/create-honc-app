e49076a2532743c38c5d36bb6cb9c9dfa5df8331
Brett Beutell <brbeut@gmail.com>
2024-10-25 21:54:45 -0700

    Remove commented out code

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -88,8 +88,6 @@ async function main() {
     ? "You can now navigate to the project folder and run the following commands to generate, apply the migrations and seed the database:"
     : "Once you've set up the database and saved the connection string, you can generate the migrations, apply them, and seed the database using the following commands";
 
-  // const dbD1 =
-  //   context.template === "sample-d1" ? `${context.packageManager} run dev` : "";
   outro(`🪿 HONC app created successfully in ${context.path}!
 
 ${dbPreamble}
