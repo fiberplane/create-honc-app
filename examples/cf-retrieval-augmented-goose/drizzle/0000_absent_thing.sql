fe7fce34ef36413348b096f4551bcbd45d00f713
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-11-06 16:59:36 +0100

    Changes

--- examples/cf-retrieval-augmented-goose/drizzle/0000_absent_thing.sql
+++ examples/cf-retrieval-augmented-goose/drizzle/0000_absent_thing.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "chunks" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"document_id" uuid NOT NULL,
