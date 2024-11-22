bb8b3f6d9e25dea452c9e911e322df73103b312c
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-11-22 11:45:12 -0800

    chore: lint

--- examples/placegoose/src/controllers/index.ts
+++ examples/placegoose/src/controllers/index.ts
@@ -1,5 +1,8 @@
 import { eq, sql } from "drizzle-orm";
-import type { SQLiteColumn, SQLiteTableWithColumns } from "drizzle-orm/sqlite-core";
+import type {
+  SQLiteColumn,
+  SQLiteTableWithColumns,
+} from "drizzle-orm/sqlite-core";
 import { HTTPException } from "hono/http-exception";
 
 import * as schema from "../db/schema";
@@ -7,24 +10,21 @@ import type { DrizzleClient } from "../types";
 
 type ColumnWithId = {
   id: SQLiteColumn;
-}
+};
 
 type SQLiteTableWithIdColumn<T extends ColumnWithId> = SQLiteTableWithColumns<{
   name: string;
   schema: undefined;
   columns: T;
   dialect: "sqlite";
-}>
+}>;
 
 export async function getRowById<T extends ColumnWithId>(
-  db: DrizzleClient, 
-  table: SQLiteTableWithIdColumn<T>, 
-  id: number
+  db: DrizzleClient,
+  table: SQLiteTableWithIdColumn<T>,
+  id: number,
 ) {
-  const rowsById = await db
-    .select()
-    .from(table)
-    .where(eq(table.id, id))
+  const rowsById = await db.select().from(table).where(eq(table.id, id));
 
   if (rowsById.length > 1) {
     new HTTPException(500, {
@@ -36,25 +36,25 @@ export async function getRowById<T extends ColumnWithId>(
 }
 
 export async function getRowExists<T extends ColumnWithId>(
-  db: DrizzleClient, 
-  table: SQLiteTableWithIdColumn<T>, 
-  id: number
+  db: DrizzleClient,
+  table: SQLiteTableWithIdColumn<T>,
+  id: number,
 ) {
   const rowExists = db.run(
-    sql`select exists (select 1 from ${table} where ${table.id} = ${id})`
-  )
+    sql`select exists (select 1 from ${table} where ${table.id} = ${id})`,
+  );
 
   return !!rowExists;
 }
 
 export async function getGaggleById(db: DrizzleClient, id: number) {
-  return getRowById(db, schema.gaggles, id)
+  return getRowById(db, schema.gaggles, id);
 }
 
 export async function getGooseById(db: DrizzleClient, id: number) {
-  return getRowById(db, schema.geese, id)
+  return getRowById(db, schema.geese, id);
 }
 
 export async function getHonkById(db: DrizzleClient, id: number) {
-  return getRowById(db, schema.honks, id)
+  return getRowById(db, schema.honks, id);
 }
