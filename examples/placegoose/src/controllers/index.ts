ab2e987e5f04424eef4bcf354a4519b2357d7c22
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-11-22 11:27:35 -0800

    chore: abstract getById logic

--- examples/placegoose/src/controllers/index.ts
+++ examples/placegoose/src/controllers/index.ts
@@ -1,52 +1,48 @@
 import { eq } from "drizzle-orm";
+import type { SQLiteColumn, SQLiteTableWithColumns } from "drizzle-orm/sqlite-core";
 import { HTTPException } from "hono/http-exception";
 
 import * as schema from "../db/schema";
 import type { DrizzleClient } from "../types";
 
-// todo: is all this necessary just to check if a record exists?
+type ColumnWithId = {
+  id: SQLiteColumn;
+}
 
-export async function getGaggleById(db: DrizzleClient, id: number) {
-  const gagglesById = await db
+type SQLiteTableWithIdColumn<T extends ColumnWithId> = SQLiteTableWithColumns<{
+  name: string;
+  schema: undefined;
+  columns: T;
+  dialect: "sqlite";
+}>
+
+async function getRowById<T extends ColumnWithId>(
+  db: DrizzleClient, 
+  table: SQLiteTableWithIdColumn<T>, 
+  id: number
+) {
+  const rowsById = await db
     .select()
-    .from(schema.gaggles)
-    .where(eq(schema.gaggles.id, id));
+    .from(table)
+    .where(eq(table.id, id))
 
-  if (gagglesById.length > 1) {
+  if (rowsById.length > 1) {
     new HTTPException(500, {
       message: "Unique Constraint Conflict",
     });
   }
 
-  return gagglesById.at(0);
+  return rowsById.at(0);
+}
+
+export async function getGaggleById(db: DrizzleClient, id: number) {
+  return getRowById(db, schema.gaggles, id)
 }
 
 export async function getGooseById(db: DrizzleClient, id: number) {
-  const geeseById = await db
-    .select()
-    .from(schema.geese)
-    .where(eq(schema.geese.id, id));
-
-  if (geeseById.length > 1) {
-    new HTTPException(500, {
-      message: "Unique Constraint Conflict",
-    });
-  }
-
-  return geeseById.at(0);
+  return getRowById(db, schema.geese, id)
 }
 
 export async function getHonkById(db: DrizzleClient, id: number) {
-  const honksById = await db
-    .select()
-    .from(schema.honks)
-    .where(eq(schema.honks.id, id));
-
-  if (honksById.length > 1) {
-    throw new HTTPException(500, {
-      message: "Unique Constraint Conflict",
-    });
-  }
-
-  return honksById.at(0);
+  return getRowById(db, schema.honks, id)
 }
