b40acbacece1cf5f3dc6d24104c697e3319d5d99
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-12-03 13:07:39 -0800

    chore: add length requirements to name (string) properties on insert

--- examples/placegoose/src/db/validation.ts
+++ examples/placegoose/src/db/validation.ts
@@ -5,11 +5,17 @@ import * as schema from "./schema";
 // The decision was made for clarity, and to exclude these
 // from the "schema" file export
 
-export const ZGaggleInsert = createInsertSchema(schema.gaggles);
+export const ZGaggleInsert = createInsertSchema(schema.gaggles, {
+    // Enhance (or override) property schemas
+    name: (schema) => schema.name.min(1),
+    territory: (schema) => schema.territory.min(1),
+});
 
 export const ZGaggleSelect = createSelectSchema(schema.gaggles);
 
-export const ZGooseInsert = createInsertSchema(schema.geese);
+export const ZGooseInsert = createInsertSchema(schema.geese, {
+    name: (schema) => schema.name.min(1),
+});
 
 export const ZGooseSelect = createSelectSchema(schema.geese);
 
