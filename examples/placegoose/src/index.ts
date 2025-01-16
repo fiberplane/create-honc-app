9bf4f81a6bdb1b5214b1171f50218d0c5bc1f543
Brett Beutell <brbeut@gmail.com>
2025-01-16 19:48:51 +0100

    Use published playground in npm package

--- examples/placegoose/src/index.ts
+++ examples/placegoose/src/index.ts
@@ -58,7 +58,7 @@ app.route("/honks", routes.honks);
 
 // Mount the Fiberplane playground to play with the API
 app.use("/fp/*", createMiddleware({
-  cdn: "https://cdn.jsdelivr.net/gh/fiberplane/fpx@playground/playground/dist/",
+  cdn: "https://cdn.jsdelivr.net/npm/@fiberplane/embedded/dist/playground/",
   // @ts-expect-error - The imported spec does not match our expected OpenAPIv3 type
   spec: apiSpec,
 }));
