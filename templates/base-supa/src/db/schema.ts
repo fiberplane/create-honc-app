12e657313bbc280af87272527fb835c6b8b740c9
ambergristle <101149854+ambergristle@users.noreply.github.com>
2025-04-30 14:49:17 +0300

    feat: add typing to schema

--- templates/base-supa/src/db/schema.ts
+++ templates/base-supa/src/db/schema.ts
@@ -3,10 +3,15 @@ import { jsonb, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 export type NewUser = typeof users.$inferInsert;
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  name: text("name"),
-  email: text("email"),
-  settings: jsonb("settings"),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+	id: serial("id").primaryKey(),
+	name: text("name"),
+	email: text("email"),
+	/**
+	 * Settings included to demonstrate json columns. Don't forget
+	 * to update the typing if you decide to use it! This is necessary
+	 * for type compatability with Hono clients.
+	 */
+	settings: jsonb("settings").$type<Record<string, unknown>>(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
