13a264a171304935b57aa2f80a8333b03aa81efa
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-05-13 21:37:38 +0530

    feat: added cli support for hono-openapi (#74)
    * feat(template): added hono-openapi template
    
    * fix(cli): updated the cli
    
    * feat(template): added supabase template
    
    * feat(template): added the D1 template
    
    * chore: minor changes
    
    * chore: minor changes based upon suggestions from @laulauland
    
    * Replace the zod-openapi templates with hono-openapi templates
    
    * Remove OpenAPITEmplate type
    
    * Do not use pnpm@8
    
    * Add back biome configs
    
    * Update deps and add back biome commands (redoing package.json based off of main)
    
    * Format templates with biome
    
    * Revert "Format templates with biome"
    
    This reverts commit a3feb4dbb0b5acbf2169034521f4eb206bcaef81.
    
    * Add biome as a devdep in the templates and run formatter on templates
    
    * Update templates/base-openapi/wrangler.toml
    
    * Update templates/base-openapi/wrangler.toml
    
    * Update templates/base-openapi/src/index.ts
    
    * Update templates/base-supa-openapi/src/index.ts
    
    * Update templates/d1-openapi/seed.ts
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Brett Beutell <brbeut@gmail.com>

--- templates/base-openapi/src/index.ts
+++ templates/base-openapi/src/index.ts
@@ -1,9 +1,13 @@
 import { neon } from "@neondatabase/serverless";
 import { eq } from "drizzle-orm";
 import { drizzle, type NeonHttpDatabase } from "drizzle-orm/neon-http";
-import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { describeRoute, openAPISpecs } from "hono-openapi";
+import { Hono } from "hono";
 import * as schema from "./db/schema";
 import { createFiberplane } from "@fiberplane/hono";
+import z from "zod";
+import { resolver, validator as zValidator } from "hono-openapi/zod";
+import "zod-openapi/extend";
 
 // Types for environment variables and context
 type Bindings = {
@@ -16,7 +20,7 @@ type Variables = {
 
 // Create the app with type-safe bindings and variables
 // For more information on OpenAPIHono, see: https://hono.dev/examples/zod-openapi
-const app = new OpenAPIHono<{ Bindings: Bindings; Variables: Variables }>();
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 
 // Middleware: Set up database connection for all routes
 app.use(async (c, next) => {
@@ -26,144 +30,154 @@ app.use(async (c, next) => {
   await next();
 });
 
-// Route Definitions
-// Each route is defined separately with its request/response schema
-// This enables automatic OpenAPI documentation and type safety
-
-const root = createRoute({
-  method: "get",
-  path: "/",
-  responses: {
-    200: {
-      content: { "text/plain": { schema: z.string() } },
-      description: "Root fetched successfully",
-    },
-  },
-});
-
 // Define the expected response shape using Zod
 //
 // We can add openapi documentation, as well as name the Schema in the OpenAPI document,
 // by chaining `openapi` on the zod schema definitions
-const UserSchema = z.object({
-  id: z.number().openapi({
-    example: 1,
-  }),
-  name: z.string().openapi({
-    example: "Nikita",
-  }),
-  email: z.string().email().openapi({
-    example: "nikita@neon.tech",
-  }),
-}).openapi("User");
-
-const getUsers = createRoute({
-  method: "get",
-  path: "/api/users",
-  responses: {
-    200: {
-      content: { "application/json": { schema: z.array(UserSchema) } },
-      description: "Users fetched successfully",
-    },
-  },
-});
-
-const NewUserSchema = z.object({
-  name: z.string().openapi({
-    example: "Nikita",
-  }),
-  email: z.string().email().openapi({
-    example: "nikita@neon.tech",
-  }),
-}).openapi("NewUser");
-
-const getUser = createRoute({
-  method: "get",
-  path: "/api/users/{id}",
-  request: {
-    // Validate and parse URL parameters
-    params: z.object({
-      id: z.string().uuid().openapi({
-        example: "123e4567-e89b-12d3-a456-426614174000",
-      }),
+const UserSchema = z
+  .object({
+    id: z.number().openapi({
+      example: 1,
     }),
-  },
-  responses: {
-    200: {
-      content: { "application/json": { schema: UserSchema } },
-      description: "User fetched successfully",
-    },
-  },
-});
+    name: z.string().openapi({
+      example: "Nikita",
+    }),
+    email: z.string().email().openapi({
+      example: "nikita@neon.tech",
+    }),
+  })
+  .openapi({ ref: "User" });
 
-const createUser = createRoute({
-  method: "post",
-  path: "/api/users",
-  request: {
-    // Validate request body using Zod schemas
-    body: {
-      required: true, // NOTE: this is important to set to true, otherwise the route will accept empty body
-      content: {
-        "application/json": {
-          schema: NewUserSchema,
+const apiRouter = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+
+apiRouter
+  .get(
+    "/",
+    describeRoute({
+      responses: {
+        200: {
+          content: {
+            "application/json": { schema: resolver(z.array(UserSchema)) },
+          },
+          description: "Users fetched successfully",
         },
       },
+    }),
+    async (c) => {
+      const db = c.get("db");
+      const users = await db.select().from(schema.users);
+      return c.json(users, 200);
     },
-  },
-  responses: {
-    201: {
-      content: {
-        "application/json": {
-          schema: UserSchema,
+  )
+  .post(
+    "/",
+    describeRoute({
+      responses: {
+        201: {
+          content: {
+            "application/json": {
+              schema: UserSchema,
+            },
+          },
+          description: "User created successfully",
         },
       },
-      description: "User created successfully",
+    }),
+    zValidator(
+      "json",
+      z
+        .object({
+          name: z.string().openapi({
+            example: "Nikita",
+          }),
+          email: z.string().email().openapi({
+            example: "nikita@neon.tech",
+          }),
+        })
+        .openapi({ ref: "NewUser" }),
+    ),
+    async (c) => {
+      const db = c.get("db");
+      const { name, email } = c.req.valid("json");
+
+      const [newUser] = await db
+        .insert(schema.users)
+        .values({
+          name,
+          email,
+        })
+        .returning();
+
+      return c.json(newUser, 201);
     },
-  },
-});
+  )
+  .get(
+    "/:id",
+    describeRoute({
+      responses: {
+        200: {
+          content: { "application/json": { schema: resolver(UserSchema) } },
+          description: "User fetched successfully",
+        },
+      },
+    }),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().uuid().openapi({
+          example: "123e4567-e89b-12d3-a456-426614174000",
+        }),
+      }),
+    ),
+    async (c) => {
+      const db = c.get("db");
+      const { id } = c.req.valid("param");
+      const [user] = await db
+        .select()
+        .from(schema.users)
+        .where(eq(schema.users.id, id));
+      return c.json(user, 200);
+    },
+  );
 
 // Route Implementations
-// Connect the route definitions to their handlers using .openapi()
-app.openapi(root, async (c) => {
-  return c.text("Honc! 🪿");
-})
-  .openapi(getUsers, async (c) => {
-    const db = c.get("db");
-    const users = await db.select().from(schema.users);
-    return c.json(users, 200);
-  })
-  .openapi(getUser, async (c) => {
-    const db = c.get("db");
-    const { id } = c.req.valid("param");
-    const [user] = await db.select().from(schema.users).where(eq(schema.users.id, id));
-    return c.json(user, 200);
-  })
-  .openapi(createUser, async (c) => {
-    const db = c.get("db");
-    const { name, email } = c.req.valid("json");
-
-    const [newUser] = await db
-      .insert(schema.users)
-      .values({
-        name,
-        email,
-      })
-      .returning();
-
-    return c.json(newUser, 201);
-  })
-  // Generate OpenAPI documentation at /openapi.json
-  .doc("/openapi.json", {
-    openapi: "3.0.0",
-    info: {
-      title: "Honc! 🪿",
-      version: "1.0.0",
-      description: "Honc! 🪿",
+app
+  .get(
+    "/",
+    describeRoute({
+      responses: {
+        200: {
+          content: { "text/plain": { schema: resolver(z.string()) } },
+          description: "Root fetched successfully",
+        },
+      },
+    }),
+    async (c) => {
+      return c.text("Honc! 🪿");
     },
-  })
-  .use("/fp/*", createFiberplane({
-    app,
-    openapi: { url: "/openapi.json" },
-  }));
+  )
+  .route("/api/users", apiRouter);
+
+// Generate OpenAPI documentation at /openapi.json
+app
+  .get(
+    "/openapi.json",
+    openAPISpecs(app, {
+      documentation: {
+        info: {
+          title: "Honc! 🪿",
+          version: "1.0.0",
+          description: "Honc! 🪿",
+        },
+      },
+    }),
+  )
+  .use(
+    "/fp/*",
+    createFiberplane({
+      app,
+      openapi: { url: "/openapi.json" },
+    }),
+  );
 
 export default app;
