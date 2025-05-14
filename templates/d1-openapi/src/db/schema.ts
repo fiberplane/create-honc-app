889e62f9c2199f5b1aa0d60a07351d503ea971a9
Brett Beutell <brbeut@gmail.com>
2025-05-14 10:39:33 +0200

    Update d1-openapi template to use uuid

--- templates/d1-openapi/src/db/schema.ts
+++ templates/d1-openapi/src/db/schema.ts
@@ -4,7 +4,8 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export type NewUser = typeof users.$inferInsert;
 
 export const users = sqliteTable("users", {
-  id: integer("id", { mode: "number" }).primaryKey(),
+  // .primaryKey() must be chained before $defaultFn
+  id: text().primaryKey().$defaultFn(() => crypto.randomUUID()),
   name: text("name").notNull(),
   email: text("email").notNull(),
   createdAt: text("created_at").notNull().default(sql`(CURRENT_TIMESTAMP)`),
