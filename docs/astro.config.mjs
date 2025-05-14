3af9ba30f5de99744b572b27655950fdb56b80ab
Brett Beutell <brbeut@gmail.com>
2025-05-14 17:37:33 +0200

    Redirect past the splash page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,41 +5,46 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/": "/get-started",
+  },
   integrations: [
-		starlight({
-			title: 'HONC Stack',
-			favicon: "/favicon/favicon.ico",
-			customCss: ['./src/styles/global.css'],
-			social: [
-				{ 
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/fiberplane/create-honc-app' 
-				},
-				{ 
-					icon: 'discord',
-					label: 'Discord',
-					href: 'https://discord.com/invite/ugAwAK6Yzm' 
-				},
-			],
-			sidebar: [
-				'get-started',
-				{
-					label: 'The Stack',
-					items: [
-						{ label: 'Hono', slug: 'stack/hono' },
-					],
-				},
-				{
-					label: 'Solutions',
-					autogenerate: { directory: 'solutions' },
-				},
-			],
-		}),
-	],
+    starlight({
+      title: "HONC Docs",
+      favicon: "/favicon/favicon.ico",
+      customCss: ["./src/styles/global.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/fiberplane/create-honc-app",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/ugAwAK6Yzm",
+        },
+      ],
+      sidebar: [
+        "get-started",
+        {
+          label: "The Stack",
+          items: [{ label: "Hono", slug: "stack/hono" }],
+        },
+        {
+          label: "Solutions",
+          autogenerate: { directory: "solutions" },
+        },
+      ],
+      expressiveCode: {
+        themes: ["github-dark", "github-light"],
+        styleOverrides: {
+          borderRadius: "var(--border-radius)",
+        },
+      },
+    }),
+  ],
   vite: {
-    plugins: [
-			tailwindcss()
-		],
+    plugins: [tailwindcss()],
   },
 });
