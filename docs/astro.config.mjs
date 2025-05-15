b207faff296ffdff13330d8f199229e065aea81a
Brett Beutell <brbeut@gmail.com>
2025-05-15 07:21:34 +0200

    HONC Docs: Style updates (#88)
    * Redirect past the splash page
    
    * Remove tailwind, fix links in nav, copy over Fiberplane site components
    
    * Fix ci
    
    * Tweak github action job name
    
    * Add starlight-package-managers dep
    
    * Add CI for deploying website
    
    * Try updating CI for preview and prod deploys
    
    * Fix CI for deploying please
    
    * Try again CI
    
    * Comment on pr with the preview

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,45 +1,52 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-		starlight({
-			title: 'HONC Stack',
-			favicon: "/favicon/favicon.ico",
-			customCss: ['./src/styles/global.css'],
-			social: [
-				{ 
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/fiberplane/create-honc-app' 
-				},
-				{ 
-					icon: 'discord',
-					label: 'Discord',
-					href: 'https://discord.com/invite/ugAwAK6Yzm' 
-				},
-			],
-			sidebar: [
-				'get-started',
-				{
-					label: 'The Stack',
-					items: [
-						{ label: 'Hono', slug: 'stack/hono' },
-					],
-				},
-				{
-					label: 'Solutions',
-					autogenerate: { directory: 'solutions' },
-				},
-			],
-		}),
-	],
-  vite: {
-    plugins: [
-			tailwindcss()
-		],
+  redirects: {
+    "/": "/get-started",
   },
+  integrations: [
+    starlight({
+      title: "HONC Docs",
+      favicon: "/favicon/favicon.ico",
+      customCss: ["./src/styles/global.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/fiberplane/create-honc-app",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/ugAwAK6Yzm",
+        },
+      ],
+      sidebar: [
+        "get-started",
+        {
+          label: "The Stack",
+          items: [{ label: "Hono", slug: "stack/hono" }],
+        },
+        {
+          label: "Solutions",
+          autogenerate: { directory: "solutions" },
+        },
+      ],
+      components: {
+        Banner: "./src/components/Banner.astro",
+        Footer: "./src/components/Footer.astro",
+        Head: "./src/components/Head.astro",
+        Header: "./src/components/Header.astro",
+      },
+      expressiveCode: {
+        themes: ["github-dark", "github-light"],
+        styleOverrides: {
+          borderRadius: "var(--border-radius)",
+        },
+      },
+    }),
+  ]
 });
