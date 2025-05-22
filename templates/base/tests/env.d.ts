494bdcf5e9ee666721c3221ad820c9f7d7816b90
ambergristle <101149854+ambergristle@users.noreply.github.com>
2025-05-22 15:24:09 +0300

    feat: set up vitest in neon template

---
+++ templates/base/tests/env.d.ts
@@ -0,0 +1,7 @@
+import type { Bindings } from '../src/index';
+
+declare module "cloudflare:test" {
+  // ProvidedEnv controls the type of `import("cloudflare:test").env`
+  interface ProvidedEnv extends Env {
+  };
+}
---
+++ templates/base/vitest.config.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import { defineWorkersConfig, readD1Migrations } from "@cloudflare/vitest-pool-workers/config";
+
+export default defineWorkersConfig(async () => {
+
+    return {
+      test: {
+        globals: true,
+        poolOptions: {
+          workers: {
+            wrangler: { configPath: "./wrangler.toml" },
+              miniflare: {
+                compatibilityFlags: ['nodejs_compat'],
+                compatibilityDate: '2024-04-01',
+              }
+          },
+        },
+      },
+      build: {
+        rollupOptions: {
+          external: ["@fiberplane/hono"],
+        }
+      },
+      ssr: {
+        noExternal: [
+          "@sagold/json-pointer",
+          "json-schema-library",
+          "@fiberplane/hono"
+        ],
+      }
+    }
+});
