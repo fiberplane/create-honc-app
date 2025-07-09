cd940e760bfc801c4f4bf6ba5af30223e74a6191
ambergristle <101149854+ambergristle@users.noreply.github.com>
2025-07-09 11:47:11 +0200

    Add target dir cli arg (#97)
    * feat: add target dir parsing to initContext
    
    * feat: only prompt for path if not included in args
    
    * feat: add target path cli script
    
    * chore: lint
    
    * chore: adjust import order
    
    * feat: collect project name rather than path
    
    * chore: lint + format
    
    * chore: remove unused await
    
    * chore: update typing
    
    * feat: update expected exit to use handleCancel

--- cli/src/actions/path.ts
+++ cli/src/actions/path.ts
@@ -1,29 +1,54 @@
+import { existsSync, readdirSync } from "node:fs";
+import { isAbsolute, parse } from "node:path";
+import { confirm, text } from "@clack/prompts";
 import type { Context } from "@/context";
-import { text } from "@clack/prompts";
+import { handleCancel } from "@/utils";
 
-// TODO: validate path
 export async function promptPath(ctx: Context) {
   try {
-    const placeholder = "./spooking-honc";
+    const defaultValue = `./${ctx.name}`;
     const result = await text({
       message: "Where should we create your project? (./relative-path)",
-      placeholder,
-      defaultValue: placeholder,
+      placeholder: defaultValue,
+      defaultValue,
       validate: (value) => {
-        if (value !== "" && value[0] !== ".") {
+        if (value === "") {
+          return undefined;
+        }
+
+        if (isAbsolute(value)) {
           return "Please enter a relative path.";
         }
+
+        if (parse(value).ext) {
+          return "Please enter a directory path.";
+        }
+
+        if (/[<>:"|?*]/.test(value) || /^\s+$/.test(value)) {
+          return "Please enter a valid path.";
+        }
+
         return undefined;
       },
     });
 
-    if (typeof result === "string") {
-      if (result === "") {
-        ctx.path = placeholder;
-      } else {
-        ctx.path = result;
+    if (typeof result === "symbol") {
+      // Process cancelled or aborted
+      return result;
+    }
+
+    if (existsSync(result) && readdirSync(result).length > 0) {
+      const confirmation = await confirm({
+        message: "Target directory isn't empty. Continue?",
+        initialValue: false,
+      });
+
+      if (!confirmation) {
+        handleCancel();
       }
     }
+
+    ctx.path = result;
 
     return result;
   } catch (error) {
--- cli/src/context.ts
+++ cli/src/context.ts
@@ -5,6 +5,7 @@ import { getPackageManager } from "./utils";
 export interface Context {
   cwd: string;
   packageManager: string;
+  name: string;
   path?: string;
   description?: string;
   template?: Template;
@@ -62,11 +63,14 @@ export interface Context {
 }
 
 export function initContext(): Context {
+  const projectName = parseProjectName(process.argv);
+
   const hatchValue = parseHatchFlag(process.argv);
   const flags: Flags = hatchValue ? ["hatch"] : [];
 
   return {
     cwd: process.cwd(),
+    name: projectName ?? "honc-app",
     packageManager: getPackageManager() ?? "npm",
     flags,
 
@@ -125,4 +129,19 @@ function parseHatchFlag(args: string[]): string | boolean {
   }
   // Otherwise, just set it to true
   return true;
+}
+
+/**
+ * Checks first (non-system) argument for existence, ignoring flags
+ * @param args - An array of command line arguments.
+ * @returns The `string` project name if matched, or `undefined`
+ */
+function parseProjectName(args: string[]): string | undefined {
+  const projectName = args.at(2);
+
+  if (!projectName || projectName.startsWith("-")) {
+    return undefined;
+  }
+
+  return projectName;
 }
